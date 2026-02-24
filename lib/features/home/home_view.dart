399235d997ee256f38faadb2995ef1deb5ca6e40
Thiago Szejka <thiagovszejka1921@gmail.com>
2026-02-24 17:25:11 -0300

    feat: add the app bar basics

--- lib/features/home/home_view.dart
+++ lib/features/home/home_view.dart
@@ -1,9 +1,57 @@
 
 import 'package:flutter/material.dart';
 
-class HomeView extends StatelessWidget{
+class HomeView extends StatefulWidget{
+  @override
+  State<HomeView> createState(){ 
+    return _HomeViewState();
+  }
+}
+
+class _HomeViewState extends State<HomeView>{
+  Color _appBarColor = Colors.lightBlue;
+  Color _titleColor = Colors.black;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text("Teste123"),),);
+    return Scaffold(
+      
+
+      appBar: AppBar(
+        title: Text('Service Hub',
+        style: TextStyle(color: _titleColor),
+        ),
+        backgroundColor: _appBarColor,
+        centerTitle: true,
+        actions: [
+          IconButton(
+            icon: Icon(Icons.notification_important),
+            onPressed: () => null,
+          ),
+          IconButton(
+            icon: Icon(Icons.dark_mode),
+            onPressed: () {
+              setState((){
+                if (_appBarColor == Colors.lightBlue){
+                  _appBarColor = Colors.black;
+                  _titleColor = Colors.white;
+                }
+                else {
+                  _appBarColor = Colors.lightBlue;
+                  _titleColor = Colors.black;
+                }
+              });
+            }
+          )
+        ],
+      ),
+      
+      body: Center(
+        child: Text(
+          "Teste123"
+        ),
+        
+      ),
+
+    );
   }
 }
